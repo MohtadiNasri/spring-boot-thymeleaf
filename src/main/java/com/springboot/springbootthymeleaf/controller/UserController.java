8f9988c2eaa293c1dbe2de8ff870195e1409b097
nasrimohtadi@gmail.com <Awkward@009>
2020-03-21 18:34:52 +0100

    Initial commit

---
+++ src/main/java/com/springboot/springbootthymeleaf/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.springboot.springbootthymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("demo")
+    public String demo(Model model) {
+        model.addAttribute("message", "Hello Thymeleaf");
+        // return to templates/demo.html page.
+        return "demo";
+    }
+}
